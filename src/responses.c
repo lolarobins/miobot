07c157161a27e9ddfcddde56744203a0752efb79
lolarobins <lola@lolarobins.ca>
2025-08-08 23:41:43 -0500

    added number choosing

--- src/responses.c
+++ src/responses.c
@@ -5,6 +5,7 @@
 // |   responses.c - bot message responses to messages mentioning it   |
 // +--- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---+
 
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -12,13 +13,9 @@
 
 #include "miobot.h"
 
-struct _response {
-    char *phrase;
-    char **list;
-    size_t len;
-};
+#define RESPONSE_MAX 4096
 
-static char *_drain_responses[] = {
+static const char *_drain_responses[] = {
     "https://tenor.com/view/"
     "bladee-drain-gang-sexy-face-bladee-cute-gif-1146993514578815466",
     "https://tenor.com/view/bladee-draingang-drain-drained-dg-gif-27078473",
@@ -30,23 +27,20 @@ static char *_drain_responses[] = {
     "https://tenor.com/view/bladee-drain-gang-silly-kitty-review-gif-27702138"
 };
 
-static char *_piss_response[] = {
+static const char *_piss_response[] = {
     "I'm sorry, due to legal restrictions, I am not able to respond to this.",
     "I am not legally allowed to respond to this message.",
     "ok so let me get this straight, i am NOT into piss, i do not have a piss "
     "kink, neither does lola, do not believe the lies that the media puts out "
     "and the rumours that are spread within this community",
-    "...."
+    "ok fr im not into it pls leave me alone"
 };
 
-static char *_insult_responses[]
-   = { "stfu idc", "\xF0\x9F\x92\x94", "wtf did i do????", "fuck you.",
-       "im sorry :((" };
+static const char *_insult_responses[]
+   = { "\xF0\x9F\x92\x94", "wtf did i do????", "fuck you.", "im sorry :((",
+       "literally you rn: <:weathersoy:879057369043197982>" };
 
-static char *_meow_responses[]
-   = { "meow!", ":3", "shut up gayass", "mrow :3", "ok homo" };
-
-static char *_is_this_true_responses[]
+static const char *_is_this_true_responses[]
    = { "no",
        "fuck no",
        "https://tenor.com/view/"
@@ -62,36 +56,118 @@ static char *_is_this_true_responses[]
        "https://tenor.com/view/"
        "true-truth-nuke-super-truth-nova-truth-meme-gif-16889273424352737553" };
 
-static char *_yes_no_responses[]
-   = { "yea", "yes", "nah", "no", "idfk dude", "idk" };
+static const char *_yes_no_responses[] = { "yea", "yes", "nah", "no", "idk" };
 
-#define _r_list(x) x, sizeof (x) / sizeof (char *)
+static const char *_pick_response_prefix[]
+   = { "im choosing ", "gonna go with ", "definitely ",
+       "going w/ ",    "probably ",      "picking " };
 
-static const struct _response _responses[]
-   = { { "drain", _r_list (_drain_responses) },
-       { "bladee", _r_list (_drain_responses) },
-       { "piss", _r_list (_piss_response) },
-       { "pee", _r_list (_piss_response) },
-       { "urine", _r_list (_piss_response) },
-       { "does", _r_list (_is_this_true_responses) },
-       { "fuck you", _r_list (_insult_responses) },
-       { "i hate you", _r_list (_insult_responses) },
-       { "you are gay", _r_list (_insult_responses) },
-       { "you're gay", _r_list (_insult_responses) },
-       { "your gay", _r_list (_insult_responses) },
-       { "kys", _r_list (_insult_responses) },
-       { "kill yourself", _r_list (_insult_responses) },
-       { "meow", _r_list (_meow_responses) },
-       { "mrow", _r_list (_meow_responses) },
-       { "nya", _r_list (_meow_responses) },
-       { "is this true", _r_list (_is_this_true_responses) },
-       { "is", _r_list (_yes_no_responses) },
-       { "am", _r_list (_yes_no_responses) },
-       { "are", _r_list (_yes_no_responses) },
-       { "does", _r_list (_yes_no_responses) },
-       { "do", _r_list (_yes_no_responses) } };
+// response storage struct
+struct _response {
+    const char *phrase;
+    const char **list;
+    size_t len;
+    bool (*callback) (char *message, char *response);
+};
 
-static int _rand (int max) { return rand () % (max + 1); }
+static int _rand_range (int min, int max) {
+    return (rand () % (max - min + 1)) + min;
+}
+static int _rand (int max) { return _rand_range (0, max); }
+
+// callback funcs to be added to struct below
+
+// pick from range or list
+bool _pick_response (char *message, char *response) {
+    int skip_len = 0;
+
+    const char *prefix = _pick_response_prefix[_rand (
+       (sizeof (_pick_response_prefix) / sizeof (char *)) - 1)];
+
+    // a number
+    if (!strncasecmp (message, "a number between ", skip_len = 17)
+        || !strncasecmp (message, "a number from ", skip_len = 14)) {
+        message += skip_len;
+
+        // pointers to start of number strings
+        char *str_1 = message, *str_2 = NULL;
+        // 1 is set to 1 so increment is not needed in if statement below
+        int str_1_len = 1, str_2_len = 1;
+
+        // read first num
+        if (!(message[0] && (message[0] == '-' || isdigit (message[0]))))
+            return false;
+        while ((++message)[0] && isdigit (message[0])) str_1_len++;
+
+        // verify grammar syntax
+        if (!(!strncasecmp (message, " and ", skip_len = 5)
+              || !strncasecmp (message, " to ", skip_len = 4)))
+            return false;
+
+        message += skip_len;
+        str_2 = message;
+
+        // read second num
+        if (!(message[0] && (message[0] == '-' || isdigit (message[0]))))
+            return false;
+        while ((++message)[0] && isdigit (message[0])) str_2_len++;
+
+        // set null pointers for using atoi
+        str_1[str_1_len] = 0, str_2[str_2_len] = 0;
+        int min = atoi (str_1), max = atoi (str_2), swap = 0;
+
+        // swap numbers if min > max
+        if (min > max) {
+            swap = min;
+            max  = min;
+            min  = swap;
+        }
+
+        snprintf (response, RESPONSE_MAX, "%s%d", prefix,
+                  _rand_range (min, max));
+
+        return true;
+    }
+
+    // between a list of items
+    if (!strncasecmp (message, "between ", 8)
+        || !strncasecmp (message, "from ", 5)
+        || !strncasecmp (message, "one: ", 5))
+        ;
+
+    return false;
+}
+
+bool _can_you (char *message, char *response) { return false; }
+
+#define _br_list(x)  x, sizeof (x) / sizeof (char *), NULL
+#define _callback(x) NULL, 0, x
+
+static const struct _response _responses[] = { // callbacks first
+    { "pick ", _callback (_pick_response) },
+    { "choose ", _callback (_pick_response) },
+    //    { "can you ", _callback (_can_you) },
+
+    // simple responses
+    { "drain", _br_list (_drain_responses) },
+    { "bladee", _br_list (_drain_responses) },
+    { "piss", _br_list (_piss_response) },
+    { "pee", _br_list (_piss_response) },
+    { "urine", _br_list (_piss_response) },
+    { "fuck you", _br_list (_insult_responses) },
+    { "i hate you", _br_list (_insult_responses) },
+    { "kys", _br_list (_insult_responses) },
+    { "bitch", _br_list (_insult_responses) },
+    { "kill yourself", _br_list (_insult_responses) },
+    { "is this true", _br_list (_is_this_true_responses) },
+    { "yes or no", _br_list (_yes_no_responses) },
+
+    { "can ", _br_list (_yes_no_responses) },
+    { "am ", _br_list (_yes_no_responses) },
+    { "are ", _br_list (_yes_no_responses) },
+    { "does ", _br_list (_yes_no_responses) },
+    { "do ", _br_list (_yes_no_responses) }
+};
 
 void responses_message_cb (struct discord *handle,
                            const struct discord_message *event) {
@@ -113,16 +189,24 @@ void responses_message_cb (struct discord *handle,
         return;
 
     // response buf
-    char response[2048];
+    char response[RESPONSE_MAX];
     response[0] = 0;
 
-    for (int i = 0; i < sizeof (_responses) / sizeof (struct _response); i++)
-        if (!strncasecmp (message, _responses[i].phrase,
-                          strlen (_responses[i].phrase))) {
-            snprintf (response, 2048, "%s",
-                      _responses[i].list[_rand (_responses[i].len - 1)]);
-            break;
+    for (int i = 0; i < sizeof (_responses) / sizeof (struct _response); i++) {
+        const struct _response *resp = &_responses[i];
+
+        if (!strncasecmp (message, resp->phrase, strlen (resp->phrase))) {
+            if (resp->callback) {
+                // give message pointer after message
+                if (resp->callback (message + strlen (resp->phrase), response))
+                    break;
+            } else {
+                snprintf (response, 2048, "%s",
+                          resp->list[_rand (resp->len - 1)]);
+                break;
+            }
         }
+    }
 
     // check if anything was written for a response
     if (!response[0]) return;
